596787e2310c37476c678370dfca4b1f77abe214
vlad <vlad@azameo.com>
2022-05-19 11:34:34 +0200

    Uploaded functionality, no changes yet

---
+++ src/reducers/index.js
@@ -0,0 +1,32 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle',
+    filters: []
+}
+
+const reducer = (state = initialState, action) => {
+    console.log('state');
+    console.log(state);
+    console.log(action);
+    switch (action.type) {
+        case 'HEROES_FETCHING':
+            return {
+                ...state,
+                heroesLoadingStatus: 'loading'
+            }
+        case 'HEROES_FETCHED':
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: 'idle'
+            }
+        case 'HEROES_FETCHING_ERROR':
+            return {
+                ...state,
+                heroesLoadingStatus: 'error'
+            }
+        default: return state
+    }
+}
+
+export default reducer;
